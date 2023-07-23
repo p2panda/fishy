38bb8d6a0935609cd0536ea8c9090780382fba68
adz <x12@adz.garden>
2023-07-23 02:06:38 +0200

    Write changes to lock file

--- src/lock_file.rs
+++ src/lock_file.rs
@@ -7,7 +7,7 @@ use p2panda_rs::entry::traits::AsEncodedEntry;
 use p2panda_rs::entry::EncodedEntry;
 use p2panda_rs::hash::Hash;
 use p2panda_rs::operation::EncodedOperation;
-use serde::{Deserialize, Serialize};
+use serde::{Deserialize, Deserializer, Serialize, Serializer};
 
 use crate::utils::files;
 
@@ -55,10 +55,44 @@ impl LockFile {
 }
 
 /// Known versions of lock file format.
-#[derive(Debug, Clone, Serialize, Deserialize)]
-#[serde(untagged, deny_unknown_fields)]
+#[derive(Debug, Clone)]
 pub enum LockFileVersion {
-    V1 = 1,
+    V1,
+}
+
+impl LockFileVersion {
+    /// Returns the operation version encoded as u64.
+    pub fn as_u64(&self) -> u64 {
+        match self {
+            LockFileVersion::V1 => 1,
+        }
+    }
+}
+
+impl Serialize for LockFileVersion {
+    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
+    where
+        S: Serializer,
+    {
+        serializer.serialize_u64(self.as_u64())
+    }
+}
+
+impl<'de> Deserialize<'de> for LockFileVersion {
+    fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
+    where
+        D: Deserializer<'de>,
+    {
+        let version = u64::deserialize(deserializer)?;
+
+        match version {
+            1 => Ok(LockFileVersion::V1),
+            _ => Err(serde::de::Error::custom(format!(
+                "unsupported lock file version {}",
+                version
+            ))),
+        }
+    }
 }
 
 /// Single commit with encoded entry and operation pair.
