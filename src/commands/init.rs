1360b47e3424bf968ec14aafc33a096cfc7c3813
adz <x12@adz.garden>
2023-09-19 22:46:47 +0200

    Make sure to not overwrite existing key

--- src/commands/init.rs
+++ src/commands/init.rs
@@ -86,7 +86,9 @@ fn init_secret_file(target_dir: &Path) -> Result<()> {
     let mut path = target_dir.to_path_buf();
     path.push(PRIVATE_KEY_FILE_NAME);
 
-    write_key_pair(&path, &key_pair)?;
+    if !path.exists() {
+        write_key_pair(&path, &key_pair)?;
+    }
 
     Ok(())
 }
